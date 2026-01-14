b0e74800672af9f7aa39e5e2d40d720172ebda01
Renato <renato.balun.ets@gmail.com>
2026-01-14 16:54:20 +0100

    prompt and changed flow

--- app/engine/index.py
+++ app/engine/index.py
@@ -1,9 +1,9 @@
 import os
 from llama_index.core.agent.workflow import FunctionAgent
 from llama_index.llms.openai import OpenAI
-
 from dotenv import load_dotenv
-from app.engine.prompt import get_technical_support_system_prompt
+from app.engine.tools.text_to_sql import TextToSQLSpec
+from app.engine.prompt import get_system_prompt
 
 load_dotenv()
 
@@ -11,17 +11,15 @@ openai_model = os.getenv("MODEL")
 openai_llm = OpenAI(model=openai_model)
 
 def get_agent():
-    wordpress_tool = WordpressSpec().to_tool_list()
-    dns_tool = DnsSpec().to_tool_list()
-    fallback_to_human_tool = FallbackToHumanSpec().to_tool_list()
+    text_to_sql_tool = TextToSQLSpec().to_tool_list()
     
-    tools = (wordpress_tool + dns_tool + fallback_to_human_tool)
+    tools = (text_to_sql_tool)
     
     agent = FunctionAgent(
         tools=tools,
         llm=openai_llm,
         verbose=True,
-        system_prompt=get_technical_support_system_prompt("English"),
+        system_prompt=get_system_prompt(),
         max_function_calls=1
     )
     
